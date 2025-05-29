8f04cf9cf8d6d108185f24be6aa041945bc12600
Seu Nome <oliveiramario343@gmail.com>
2025-05-29 20:36:41 -0300

    mudança conexao caruaru

--- scripts.js
+++ scripts.js
@@ -6,10 +6,10 @@ function toggleMode(){
   const img = document.querySelector("#profile img")
 
   if(html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "./assets/conexão-caruaru.png")
 
   } else{
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/conexão-caruaru.png")
 
   }
 }
